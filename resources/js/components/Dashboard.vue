68074adbe865d870a6d273c17f0f36f6a44d6acf
benjamin-andoh <benoo.andoh@gmail.com>
2021-03-23 17:22:21 +0000

    added .veu() postCss webpask

---
+++ resources/js/components/Dashboard.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card card-default">
+                    
+                    <div class="card-header"> 
+                        Dashboard Component
+                     </div>
+                        
+                    <div class="card-body">
+                        I'm an Dashboard Component
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        mounted(){
+            console.log('Component mounted.')
+        }
+    }
+</script>
