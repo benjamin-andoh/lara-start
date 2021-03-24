ffad6638f9d238d59248ceb729a4c2afacbedf84
benjamin-andoh <benoo.andoh@gmail.com>
2021-03-24 09:44:40 +0000

    added a vue router

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,7 @@ require('./bootstrap');
 
 window.Vue = require('vue').default;
 
+import Vue from 'vue';
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -19,6 +20,18 @@ window.Vue = require('vue').default;
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+let routes = [
+    {path:"/dashboard", component: require("./components/Dashboard.vue").default },
+    {path:"/profile", component: require("./components/Profile.vue").default}
+]
+
+const router = new VueRouter(
+    {routes}
+)
+
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 /**
@@ -29,4 +42,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    router
 });
