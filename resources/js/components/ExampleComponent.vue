a295874aad737a4e79a8816ae46246432c6cd3eb
benjamin-andoh <benoo.andoh@gmail.com>
2021-03-24 08:21:27 +0000

    install laravel vue ui

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -2,16 +2,12 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card card-default">
-                    
-                    <div class="card-header"> 
-                        Example Component
-                     </div>
-                        
-                    <div class="card-body">
-                        I'm an example Component
-                    </div>
+                <div class="card">
+                    <div class="card-header">Example Component</div>
 
+                    <div class="card-body">
+                        I'm an example component.
+                    </div>
                 </div>
             </div>
         </div>
@@ -19,8 +15,8 @@
 </template>
 
 <script>
-    export default{
-        mounted(){
+    export default {
+        mounted() {
             console.log('Component mounted.')
         }
     }
